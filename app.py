1e10a713d242658fc1a5574dd425b7920ad6961a
samuel.tin <samuel.tin@gmail.com>
2023-03-31 14:28:59 +0100

    add execution env

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 app = Flask(__name__)
+import os
+from dotenv import load_dotenv
 
 
 @app.route('/')
@@ -16,14 +18,16 @@ def favicon():
 @app.route('/hello', methods=['POST'])
 def hello():
    name = request.form.get('name')
+   env = os.getenv('EXECUTION_ENV')
 
    if name:
        print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
+       return render_template('hello.html', name = name, env = env)
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
+   load_dotenv()
    app.run()
